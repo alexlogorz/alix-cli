195e04c0dc765833e650caca041faf87d16833d8
alex logorz <alex.logorz@gmail.com>
2024-06-05 16:40:00 -0400

    updated gitignore, tsconfig and deleted old js files that were migrated to typescript

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { TitleCommand } from "./commands/TitleCommand";
 import { HelpCommand } from "./commands/HelpCommand";
 import { CleanCommand } from "./commands/CleanCommand";
 import { ImageCommand } from "./commands/ImageCommand";
+import { SetCommand } from "./commands/SetCommand";
 
 const commands: ICommand[] = [ 
     new TitleCommand('-t'), 
     new HelpCommand('help'),
     new CleanCommand('clean'),
-    new ImageCommand('-p')
+    new ImageCommand('-p'),
+    new SetCommand('set')
  ]
 const alix = new AlixCLI(commands);
 const args = process.argv.slice(2)
