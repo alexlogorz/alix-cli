b80f26392492f0682d3fc49ddd944f73a3b6072e
Alex Logorz <alex.logorz@gmail.com>
2024-06-11 15:59:55 -0400

    fixed the installer

--- AlixCLI-Win-Installer-V1.0.bat
+++ AlixCLI-Win-Installer-V1.0.bat
@@ -1,58 +1,83 @@
 @echo off
 
-net session >nul 2>&1
+:: Initializing script variables.
+
+set "NODE_VERSION=20.14.0"
+set "NODE_INSTALL_DIR=C:\Program Files\nodejs"
+set "ALIX_INSTALL_DIR=%USERPROFILE%"
+set "NPM_GLOBAL_DIR=%USERPROFILE%\AppData\Roaming\npm"
+
+:: Checking for admin privileges.
+
+net session >null 2>&1
 
 if %errorlevel% neq 0 (
-    echo This script requires administrative privileges. Please exit, right click the installer and "Run as Administrator".
-    pause
-    exit
-) 
-
-echo Checking node version...
-
-node --version >nul 2>&1
-
-REM Admin privileges was needed for Node installation here. Now we can install.
-if %errorlevel% neq 0 (
-    echo Node is not installed. Installing...
-    curl -o node-v20.14.0-x64.msi https://nodejs.org/dist/v20.14.0/node-v20.14.0-x64.msi
-    msiexec /i node-v20.14.0-x64.msi /quiet /qn /norestart
-    echo Node installation completed.
-    del node-v20.14.0-x64.msi
-) else (
-    echo Node is already installed. Skipping installation...
+	echo Admin privileges are required in order to proceed. Please exit, right-click and "Run as Administrator".
+	del null
+	cmd /k
 )
 
-set "INSTALL_DIR=%USERPROFILE%"
+:: Checking if Node Js is installed.
 
-if not exist "%INSTALL_DIR%\alix-cli-main" (
-    echo Installing alix-cli-main...
+echo Checking for Node Js...
 
-    curl -o alix-cli-main.zip -L https://github.com/alexlogorz/alix-cli/archive/main.zip
+node --version >null 2>&1
 
-    powershell -Command "Expand-Archive -Path .\alix-cli-main.zip -DestinationPath \"%INSTALL_DIR%\""
-
-    del alix-cli-main.zip
+if %errorlevel% neq 0 (
+   	echo Node Js is not installed. Installing...
+   
+	curl -o node-v%NODE_VERSION%-x64.msi https://nodejs.org/dist/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
+	msiexec /i node-v%NODE_VERSION%-x64.msi /quiet /qn /norestart
+	del node-v%NODE_VERSION%-x64.msi
 	
-    echo Navigating to alix-cli...
+	echo Node installation completed.
 
-    cd /d "%INSTALL_DIR%\alix-cli-main"
+	:: Refresh for future session.
+   	setx PATH "%PATH%;%NODE_INSTALL_DIR%" /M
+    	
+    	:: Refresh for current session.
+    	set "PATH=%PATH%;%NODE_INSTALL_DIR%"
 
-    echo Installing dependencies...
-
-    call npm install
-
-    echo Running build script...
-
-    call npm run build
-
-    echo Installing the package globally...
-
-    call npm install -g .	
-
-    echo Alix CLI has been installed. Type alix help for more info.
 ) else (
-    echo alix-cli-main is already installed. Skipping installation.
+	echo Node Js is already installed...
 )
 
-cmd /k
+if not exist "%ALIX_INSTALL_DIR%\alix-cli-main" (
+	echo Installing Alix...
+
+	:: Downloading the zip file
+	curl -o alix-cli-main.zip -L https://github.com/alexlogorz/alix-cli/archive/main.zip
+
+	:: Extracting the zip file using tar
+	tar -xf alix-cli-main.zip -C "%ALIX_INSTALL_DIR%"
+
+	:: Deleting the zip file
+	del alix-cli-main.zip
+
+	echo Extraction completed... Navigating to alix-cli-main.
+	
+	cd /d "%ALIX_INSTALL_DIR%\alix-cli-main"
+
+	echo Installing dependencies...
+	
+	call npm install
+
+	echo Building package...
+
+	call npm run build
+
+	echo Installing the package globally...
+
+	call npm install -g .
+
+	:: Refresh for future session.
+   	setx PATH "%PATH%;%NPM_GLOBAL_DIR%" /M
+    	
+    	:: Refresh for current session.
+    	set "PATH=%PATH%;%NPM_GLOBAL_DIR%"
+
+) else (
+	echo Alix is already installed...
+)
+
+cmd /k "alix help"
