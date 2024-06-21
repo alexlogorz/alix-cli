a62fd79eccffc28e13837517f411e9ee263dc5d5
Felipe Ferreira <felipedferreira11@gmail.com>
2024-06-21 11:38:52 -0400

    chore: addressing merge issues

--- src/commands/TitleFunction.ts
+++ src/commands/TitleFunction.ts
@@ -1,15 +1,19 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 import { IFunction } from './../models/IFunction';
-import { ExecuteFunctionException } from "./../models/ExecuteFunctionException";
-import { ParamNotFoundException } from "./../models/ParamNotFoundException";
+import { ExecuteFunctionException } from './../models/ExecuteFunctionException';
+import { ParamNotFoundException } from "../models/ParamNotFoundException";
+import { GenerateContentResult, GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai";
 
 export class TitleFunction implements IFunction {
     public name: string;
     
     private param?: string;
+    private model: GenerativeModel;
 
     constructor() {
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "")
         this.name = 'title'
+        this.model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
     }
 
     public setParam(value: string): void {
@@ -34,16 +38,17 @@ export class TitleFunction implements IFunction {
 
                 return titleElement.textContent || ""
             });
-            
+
             await browser.close(); 
 
-            return `\x1b[32mProduct title:\x1b[0m ${title}`
+            const prompt: string = `Given this product title: ${title}. Re write it and make it between 70-100 characters in length. Do not include any formatting in your answer.`
+            const result: GenerateContentResult = await this.model.generateContent(prompt)
+            const generatedTitle = await result.response.text()
+
+            return `\x1b[32mProduct title (Generative AI):\x1b[0m ${generatedTitle}`
         }
         catch(error: any) {
             throw new ExecuteFunctionException(error.message)
         }
     }
-
-    
-    
 }
